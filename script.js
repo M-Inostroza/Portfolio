dc1be338518504956ca386f9602a24db71d15470
Mauricio Inostroza <mautrode@gmail.com>
2022-04-16 13:52:31 +0200

    Update anims

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var anim = bodymovin.loadAnimation({
+
+// Planet works animation
+var planet_works = bodymovin.loadAnimation({
     container: document.getElementById("planet-works"),
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '/anims/data.json'
+    path: '/anims/planet_works.json'
 })
+
+
+ 
