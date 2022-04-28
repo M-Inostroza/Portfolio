a0ed94bad10848b21dde1104012d8a12aae3d654
Mauricio Inostroza <mautrode@gmail.com>
2022-04-28 10:54:09 +0200

    animejs first test

--- animeCode.js
+++ animeCode.js
@@ -2,8 +2,54 @@
 
 const jumpWords = ["make", "design", "prototype", "build", "imagine"];
 
+const textElement = document.querySelector('.description-jumper')
 
-anime({
-    targets: 'span',
-    translateY: -250
-  });
+let textIndex = 0;
+
+const textTimeline = anime.timeline({
+  easing: 'easeOutQuad',
+  loop: true
+});
+
+textTimeline.add({
+  targets: textElement,
+  begin: function() {textElement.innerText = jumpWords[textIndex]},
+  translateY: [0, -40],
+  duration: 1500,
+  opacity: 0,
+  complete: function() {textIndex++}
+}).add({
+  targets: textElement,
+  begin: function() {textElement.innerText = jumpWords[textIndex];},
+  translateY: [0, -40],
+  duration: 1500,
+  opacity: [1, 0],
+  complete: function() {textIndex++}
+}).add({
+  targets: textElement,
+  begin: function() {textElement.innerText = jumpWords[textIndex]},
+  translateY: [0, -40],
+  duration: 1500,
+  opacity: [1, 0],
+  complete: function() {textIndex++}
+}).add({
+  targets: textElement,
+  begin: function() {textElement.innerText = jumpWords[textIndex]},
+  translateY: [0, -40],
+  duration: 1500,
+  opacity: [1, 0],
+  complete: function() {textIndex++}
+}).add({
+  targets: textElement,
+  begin: function() {textElement.innerText = jumpWords[textIndex]},
+  translateY: [0, -40],
+  duration: 1500,
+  opacity: [1, 0],
+  complete: function() {textIndex = 0}
+})
+
+
+
+
+
+
