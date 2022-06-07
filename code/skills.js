79ebf5fae775451ff17fefa0ab521a3531adfeed
Mauricio Inostroza <mautrode@gmail.com>
2022-06-07 07:44:47 +0200

    skills frame update

--- code/skills.js
+++ code/skills.js
@@ -1,12 +1,33 @@
 const [...faders] = document.querySelectorAll('.skill-fader');
+const [...tabTitles] = document.querySelectorAll('.tab-title')
 
-const checkView = (element) => {
-    const rect = element.getBoundingClientRect();
-    console.log(rect)
-}
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        var translateX_Distance;
+        switch (entry.target.dataset.name) {
+            case 'ps':
+                translateX_Distance = 600
+                break;
+            case 'ai':
+                translateX_Distance = 500
+                break;
+            case 'ae':
+                translateX_Distance = 650 
+                break;
+            case 'fi':
+                translateX_Distance = 700
+                break;
+        }
+        anime({
+            targets: entry.target,
+            translateX: [0, translateX_Distance],
+            easing: 'easeInOutQuad',
+            duration: 850,
+            delay: 200
+        })
+    })
+});
 
-checkView(faders[0])
-
-faders[0].addEventListener('click', () =>{
-    console.log('changing')
-})
+faders.forEach(fader => {
+    observer.observe(fader)
+});
