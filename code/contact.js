76ec11be94f14708d3d72ce03b65f46a40372621
Mauricio Inostroza <mautrode@gmail.com>
2022-06-15 11:21:53 +0200

    general update

--- code/contact.js
+++ code/contact.js
@@ -2,18 +2,37 @@
 const submit_button = document.querySelector('.submit-button');
 
 const name_input = document.getElementById('name');
+const lastName_input = document.getElementById('lastName');
+const email_input = document.getElementById('email');
+const message_input = document.getElementById('message');
 
+const form = document.querySelector('.grid-form')
 
-//Data object
-let form_data = {
-    name: "",
-    lastName: "",
-    email: "",
-    message: ""
-}
+let can_submit;
+document.addEventListener('load', ()=>{
+    !can_submit
+})
 
-//Get data
-name_input.addEventListener('keyup', (e)=> {
-    form_data.name = e.target.value
-    console.log(form_data)
+form.addEventListener('keyup', ()=> {
+    if (name_input.value === '' || name_input.value == null
+        || lastName_input.value === '' || lastName_input.value == null
+        || email_input.value === '' || email_input.value == null
+        || message_input.value === '' || message_input.value == null)
+        {
+        can_submit = false;
+        submit_button.classList.add('disabled');
+        submit_button.classList.remove('enabled');
+    }
+    else {
+        can_submit = true;
+        submit_button.classList.add('enabled')
+        submit_button.classList.remove('disabled')
+        console.log('all good')
+    }
+})
+
+form.addEventListener('submit', (e)=>{
+    if (!can_submit) {
+        e.preventDefault()
+    }
 })
